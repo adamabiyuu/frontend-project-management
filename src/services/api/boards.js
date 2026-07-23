9a7eeaefcdd076481b85a45295aa6dbe1d71f3a7
adamabiyuu <advmabiyu@gmail.com>
2026-07-23 20:46:15 +0700

    feat: debugging kali ya

---
+++ src/services/api/boards.js
@@ -0,0 +1,21 @@
+// import network from '@/utils/network';
+
+// const boards = {
+//   async myBoards() {
+//     return network.get('/api/v1/boards');
+//   },
+// };
+
+// export default boards;
+
+import network from "@/utils/network";
+
+const boards = {
+    async myBoards(params) {
+        return network.get('/api/v1/boards/my', {
+            params,
+        });
+    }
+};
+
+export default boards;
